afff5a38639fe1e30a8897b61399239f2ba8c847
MuslimRazaa <89872278+MuslimRazaa@users.noreply.github.com>
2024-05-04 16:47:15 +0500

    image uploader done

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 var express = require("express");
 var app = express();
 app.use(express.json());
-
+app.set("view engine", "ejs");
+let ejs = require('ejs');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const path = require('path')
 var connection = require("./database");
 
 const db = require("./models")
-
 const {User} = require("./models");
 
 // Import the Package model and pass sequelize
 const PackageModel = require("./models/Package");
 const Package = PackageModel(db.sequelize, db.Sequelize); // Pass sequelize and DataTypes to the Package model
 
+
 // ------------
+
+
+app.post('/uploadImage', upload.single('file'), function (req, res, next) {
+    // Check if a file was uploaded successfully
+    if (!req.file) {
+        // If no file was uploaded, return an error response
+        return res.status(400).send('Error uploading file');
+    }
+    
+    // If a file was uploaded successfully, return a success response
+    console.log("file uploaded")
+    return res.json(req.file);
+});
 
 
 app.get('/getUser/:id', function(req, res){
@@ -29,29 +46,6 @@ app.get('/getUser/:id', function(req, res){
             console.error(err);
             res.status(500).send('Internal Server Error'); // If an error occurs, send a 500 response
         });
-});
-
-app.post('/addUser', function(req, res){
-    console.log('body',req.body)
-    User.create({
-        firstName: req.body.firstName,
-        email: req.body.email,
-        password:req.body.password,
-        roleId: req.body.roleId,
-        packageId: req.body.packageId,
-        deletedAt: req.body.deletedAt,
-        deletedBy: req.body.deletedBy,
-        createdBy:req.body.createdBy,
-        updatedBy: req.body.updatedBy,
-        isDeleted: req.body.isDeleted,
-    })
-    .then(() => {
-        res.send("User Added successfully")
-    })
-    .catch((err) =>{
-    console.error(err); // Log the error
-})
-    
 });
 
 app.post('/addPackage', function(req, res) {
@@ -99,6 +93,7 @@ app.get('/getPackage/:id', function(req, res){
             res.status(500).send('Internal Server Error'); // If an error occurs, send a 500 response
         });
 });
+
 
 
 db.sequelize.sync().then((req) =>{
